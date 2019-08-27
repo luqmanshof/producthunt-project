c930e55d9981be1d55c945fc489eb8be3b4ff347
Luqman <luqmanshof@gmail.com>
2019-08-27 23:09:20 +0100

    project first commit

---
+++ producthunt/urls.py
@@ -0,0 +1,12 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls import url
+# from products import views
+import products.views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',products.views.home, name='home'),
+    path('accounts/',include('accounts.urls')),
+]
